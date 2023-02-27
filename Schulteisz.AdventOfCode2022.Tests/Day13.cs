7ea10626304cdc39f91c5db0526433dd3cac6d9d
Adam Schulteisz <adam.schulteisz@evosoft.com>
2023-02-27 09:20:08 +0100

    Add day Day13 test

---
+++ Schulteisz.AdventOfCode2022.Tests/Day13.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Schulteisz.AdventOfCode2022.Interfaces;
+using System.Collections.Generic;
+
+namespace Schulteisz.AdventOfCode2022.Tests
+{
+    [TestClass]
+    internal class Day13
+    {
+        [TestMethod]
+        public void Task1() 
+        {
+        }
+
+        [TestMethod]
+        public void Task1Predefined()
+        {
+        }
+
+        [TestMethod]
+        public void Task2()
+        {
+        }
+
+        [TestMethod]
+        public void Task2Predefined()
+        {
+        }
+
+        internal class PredefinedContentParser : IContentParser
+        {
+            public List<string> GetLines(string manifestName)
+            {
+                return new List<string>()
+                {
+                    
+                };
+            }
+        }
+    }
+}
